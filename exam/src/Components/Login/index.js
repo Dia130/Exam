006daea355b5b30a70e75773afa558b965ec7f9a
Dia130 <garaevanigina@gmail.com>
2023-04-20 12:26:13 +0500

    Added exam

--- exam/src/Components/Login/index.js
+++ exam/src/Components/Login/index.js
@@ -2,9 +2,9 @@ import './Style.css'
 
 function Login(){
     return(
-        <div className="Main">
-            <input type="text" placeholder="Enter nickname or email" /> <br/>
-            <input type="password" placeholder="Enter password" /><br/>
+        <div className="main">
+            <input type="text" placeholder="Enter nickname or email" />
+            <input type="password" placeholder="Enter password" />
             <button>Enter</button> 
         </div>
     )
